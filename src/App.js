461b21c8a9e12085afc9afc457dca7a9982797f8
taesung1993 <cheonyulin@gmail.com>
2022-10-27 11:51:03 +0900

    feat: commit-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+
 function App() {
-  const message = 'Hello, world';
+  const message = 'Hello, world1';
   return (
     <div className="App">
-      <h1>Hello, World</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
